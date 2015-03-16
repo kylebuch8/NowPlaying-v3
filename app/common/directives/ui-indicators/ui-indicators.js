026618a112d71c8cf3ef1d6ef43ab31d1bfa9c0f
Kyle Buchanan <kylebuch8@gmail.com>
2015-03-15 20:16:27 -0400

    animation detail fix

--- app/common/directives/ui-indicators/ui-indicators.js
+++ app/common/directives/ui-indicators/ui-indicators.js
@@ -2,9 +2,17 @@
     'use strict';
 
     /*global angular*/
+    var circles = [];
+
     function UiPageIndicators() {
+        var self = this;
+
         this.active = 0;
         this.numIndicators = 0;
+
+        document.body.addEventListener('pagechange', function (event) {
+            self.activate(event.detail);
+        });
 
         this.setNumIndicators = function (number) {
             this.numIndicators = number;
@@ -15,7 +23,10 @@
         };
 
         this.activate = function (number) {
+            circles[this.active].removeClass('active-page');
             this.active = number;
+            circles[this.active].addClass('active-page');
+
             return number;
         };
     }
@@ -32,34 +43,33 @@
                         length = attr.number,
                         active = parseInt(attr.active, 10),
                         circleEl = '<div class="circle"></div>',
-                        circles = [],
                         newCircleEl;
 
                     for (i; i < length; i += 1) {
                         newCircleEl = angular.element(circleEl);
 
                         if (i === active) {
-                            newCircleEl.addClass('active');
+                            newCircleEl.addClass('active-page');
                         }
 
                         element.append(newCircleEl);
                         circles.push(newCircleEl);
                     }
 
-                    scope.uiPageIndicators.setNumIndicators(circles.length);
-                    scope.active = active;
-
-                    scope.$watch('uiPageIndicators.active', function (newValue) {
-                        if (newValue  || newValue === 0) {
-                            if (newValue === scope.active) {
-                                return;
-                            }
-
-                            circles[scope.active].removeClass('active');
-                            scope.active = newValue;
-                            circles[scope.active].addClass('active');
-                        }
-                    });
+                    // scope.uiPageIndicators.setNumIndicators(circles.length);
+                    // scope.active = active;
+                    //
+                    // scope.$watch('uiPageIndicators.active-page', function (newValue) {
+                    //     if (newValue  || newValue === 0) {
+                    //         if (newValue === scope.active) {
+                    //             return;
+                    //         }
+                    //
+                    //         circles[scope.active].removeClass('active-page');
+                    //         scope.active = newValue;
+                    //         circles[scope.active].addClass('active-page');
+                    //     }
+                    // });
                 }
             };
         }])
